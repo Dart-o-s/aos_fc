b29cf94775dc77ad969ba4d2c9fbdf1f4e6102c4
aos <angelo.schneider@gmail.com>
2025-06-12 19:54:31 +0700

    Removed the error to push.

--- lib/fc_objectbox.dart
+++ lib/fc_objectbox.dart
@@ -2,7 +2,7 @@
 // only small difference is that we try to upgrade our model here a bit, while we
 // introduce objectbox, and add a name and a description to the class
 import 'package:objectbox/objectbox.dart';
-import 'objectbox.g.dart'; // created by $ flutter pub run build_runner build, note: add this to pubspec.yaml - 'build_runner: ^2.4.9'
+// import 'objectbox.g.dart'; // created by $ flutter pub run build_runner build, note: add this to pubspec.yaml - 'build_runner: ^2.4.9'
                            // or use: $ dart run build_runner build
 @Entity()
 class FlashCardFile {
